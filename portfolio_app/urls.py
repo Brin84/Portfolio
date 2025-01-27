2726f0c8402b9d98b79a0fa4b16621ac88febe2e
Alex84 <brin1984@mail.ru>
2025-01-27 20:59:30 +0300

    Добавил css, настроил маршруты в классах представления, добавил html

--- portfolio_app/urls.py
+++ portfolio_app/urls.py
@@ -5,7 +5,7 @@ app_name = "portfolio_app"
 
 urlpatterns = [
     path("", views.HomeView.as_view(), name="home"),
-    path("projects/", views.ProjectListView.as_view(), name="project"),
-    path("articles/", views.ArticleListView.as_view(), name="article"),
+    path("projects/", views.ProjectListView.as_view(), name="projects"),
+    path("articles/", views.ArticleListView.as_view(), name="articles"),
     path("contact/", views.ContactView.as_view(), name="contact")
 ]
--- portfolio_app/views.py
+++ portfolio_app/views.py
@@ -5,25 +5,35 @@ from django.http import HttpResponse
 from django.views.generic import TemplateView, View, ListView
 from .models import Project
 
-class HomeView(View):
-    # template_name = 'portfolio_app/home.html'
-    def get(self, request):
-        return HttpResponse("Это главная страница")
+class HomeView(TemplateView):
+    template_name = 'portfolio_app/home.html'
+    # def get(self, request):
+    #     return HttpResponse("Это главная страница")
 
 
-class ProjectListView(View):
-    # model = Project
-    # template_name = 'portfolio_app/home.html'
-    # context_object_name = "project"
-    def get(self, request):
-        return HttpResponse("Это страница проектов")
+class ProjectListView(ListView):
+    model = Project
+    template_name = 'portfolio_app/projects.html'
+    context_object_name = "projects"
+    # def get(self, request):
+    #     return HttpResponse("Это страница проектов")
 
 
-class ArticleListView(View):
-    def get(self, request):
-        return HttpResponse("Это страница статьи")
+class ArticleListView(ListView):
+    model = Project
+    template_name = 'portfolio_app/articles.html'
+    context_object_name = "articles"
 
 
 class ContactView(View):
     def get(self, request):
-        return HttpResponse("Это страница обратной связи")
+        context = {'success': False}
+        return render(request, 'portfolio_app/contact.html', context)
+
+    def post(self, request):
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+
+        context = {'success': True}
+        return render(request, 'portfolio_app/contact.html', context)
