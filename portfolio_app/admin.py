9b05cb7af1e52b56e71a152b16b553451b221b7d
Alex84 <brin1984@mail.ru>
2025-02-19 16:08:53 +0300

    добавил комментарии в файлах admin.py, apps.py, urls.py

--- portfolio_app/admin.py
+++ portfolio_app/admin.py
@@ -12,17 +12,31 @@ class ProjectAdmin(admin.ModelAdmin):
     ordering = ('-created_at',)  # Сортировка от новых к старым
 
 
-@admin.register(Article)
-class ArticleAdmin(admin.ModelAdmin):
-    list_display = ('title', 'image', 'created_at')
-    search_fields = ('title', 'content')
-    list_filter = ('created_at',)
-    ordering = ('-created_at',)
+@admin.register(
+    Article)  # Декоратор, регистрирующий модель Article в админ-панели Django, используя класс настройки ArticleAdmin.
+class ArticleAdmin(
+    admin.ModelAdmin):  # Определяем класс ArticleAdmin, наследуя от admin.ModelAdmin,
+    # для настройки отображения модели Article в админке.
+    list_display = (
+        'title', 'image',
+        'created_at')  # Перечисляем поля модели, которые будут отображаться в списке объектов в админке.
+    search_fields = ('title', 'content')  # Указываем поля, по которым будет осуществляться поиск в админке.
+    list_filter = ('created_at',)  # Указываем поля, по которым можно будет фильтровать объекты в админке.
+    ordering = (
+        '-created_at',)  # Задаем порядок сортировки объектов в админке, начиная с самых последних по дате создания
+    # (обратный порядок).
 
 
-@admin.register(ContactMessage)
-class ContactMessageAdmin(admin.ModelAdmin):
-    list_display = ('name', 'email', 'created_at',)
-    search_fields = ('name', 'email')
-    list_filter = ('created_at',)
-    ordering = ('-created_at',)
+@admin.register(
+    ContactMessage)  # Декоратор регистрирует модель ContactMessage в админ-панели Django
+# с использованием класса ContactMessageAdmin.
+class ContactMessageAdmin(
+    admin.ModelAdmin):  # Определяем класс ContactMessageAdmin, наследуя от admin.ModelAdmin,
+    # для настройки отображения модели ContactMessage в админке.
+    list_display = (
+    'name', 'email', 'created_at',)  # Указываем поля, которые будут отображаться в списке объектов в админке.
+    search_fields = ('name', 'email')  # Определяем поля, по которым будет производиться поиск в админке.
+    list_filter = ('created_at',)  # Указываем поле, по которому можно фильтровать объекты в админке.
+    ordering = (
+    '-created_at',)  # Устанавливаем порядок сортировки объектов в админке, начиная с последних по дате создания
+    # (в обратном порядке).
